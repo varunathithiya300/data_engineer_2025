ea26060ea6658619c533f665295a97a713f945a1
Varun Athithiya <shenbagarajvarun@gmail.com>
2025-06-19 11:16:29 +0530

    pipe, getattr, callable

--- python/pandas/pd_practice.py
+++ python/pandas/pd_practice.py
@@ -7,31 +7,58 @@ hotel_df = pd.read_csv('D:\Varun Athithiya\DE_ROADMAP_2025\data_engineer_2025\py
                 infer_datetime_format=True,
                 float_precision="high")
 
-hotel_df.columns = hotel_df.columns.str.lower()
+# hotel_df.columns = hotel_df.columns.str.lower()
 # hote_df.columns = hotel_df.columns.apply(lambda x: x.lower())
 
 # filtering the dataset based on a regex pattern for date
-hotel_df = hotel_df[hotel_df['date'].str.contains('^[0-9]{1,2}-[a-zA-Z]{1,9}-[0-9]{1,4}$', regex=True, na=False)]
+# hotel_df = hotel_df[hotel_df['date'].str.contains('^[0-9]{1,2}-[a-zA-Z]{1,9}-[0-9]{1,4}$', regex=True, na=False)]
 
 # converting the field to datetime
-hotel_df['date'] = pd.to_datetime(hotel_df['date'], dayfirst=True)
-hotel_df['date'] = hotel_df['date'].dt.tz_localize('UTC')
+# hotel_df['date'] = pd.to_datetime(hotel_df['date'], dayfirst=True)
+# hotel_df['date'] = hotel_df['date'].dt.tz_localize('UTC')
 
 # extracting date components from the date field
-hotel_df['year'] = hotel_df['date'].dt.year
-hotel_df['month'] = hotel_df['date'].dt.month
-hotel_df['day'] = hotel_df['date'].dt.day
-hotel_df['month_name'] = hotel_df['date'].dt.month_name()
-hotel_df['day_name'] = hotel_df['date'].dt.day_name()
-hotel_df['timestamp'] = hotel_df['date'].apply(lambda x: x.timestamp())
-hotel_df['first_name'] = hotel_df['person name'].apply(lambda x: x.split(' ')[0])
-hotel_df['last_name'] = hotel_df['person name'].apply(lambda x: x.split(' ')[1])
+# hotel_df['year'] = hotel_df['date'].dt.year
+# hotel_df['month'] = hotel_df['date'].dt.month
+# hotel_df['day'] = hotel_df['date'].dt.day
+# hotel_df['month_name'] = hotel_df['date'].dt.month_name()
+# hotel_df['day_name'] = hotel_df['date'].dt.day_name()
+# hotel_df['timestamp'] = hotel_df['date'].apply(lambda x: x.timestamp())
+# hotel_df['first_name'] = hotel_df['person name'].apply(lambda x: x.split(' ')[0])
+# hotel_df['last_name'] = hotel_df['person name'].apply(lambda x: x.split(' ')[1])
 
 # print(hotel_df)
-hotel_df['first_name'] = hotel_df['first_name'].apply(lambda x: x.lower())
-hotel_df['last_name'] = hotel_df['last_name'].apply(lambda x: x.lower())
+# hotel_df['first_name'] = hotel_df['first_name'].apply(lambda x: x.lower())
+# hotel_df['last_name'] = hotel_df['last_name'].apply(lambda x: x.lower())
 
 # print(type(hotel_df.columns))
 # print(type(hotel_df['date']))
 # print(type(hotel_df['first_name']))
-print(hotel_df)
+# print(hotel_df)
+
+def splitTime(df, date_field, component):
+    df = df.dropna()
+    df[date_field] = pd.to_datetime(df[date_field])
+    # df[component] = df[date_field].dt.component
+    dt_accessor = getattr(df[date_field].dt, component)
+    df[component] = dt_accessor() if callable(dt_accessor) else dt_accessor
+    return df
+
+def computeTimeComponents(df, date_field):
+    return(
+        df
+        .pipe(splitTime, date_field, 'year')
+        .pipe(splitTime, date_field, 'month')
+        .pipe(splitTime, date_field, 'day')
+        .pipe(splitTime, date_field, 'day_name')
+        .pipe(splitTime, date_field, 'month_name')
+    )
+
+print(computeTimeComponents(hotel_df, 'Date'))
+
+# hotel_df = hotel_df.dropna()
+# if callable(pd.to_datetime(hotel_df['Date']).dt.year):
+#     print(True)
+# else:
+#     print(False)
+
