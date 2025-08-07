c644b20a69bd714d38f1d64d3f5c195fc2de9232
Varun Athithiya <varunathithiya.shenbagaraj@octavebio.com>
2025-08-07 08:20:47 +0530

    sync up

--- python/dsa/sorting_algo/merge_sort.py
+++ python/dsa/sorting_algo/merge_sort.py
@@ -1,17 +1,14 @@
 def merge_sort(arr):
     if len(arr) <= 1:
         return arr
-    
     mid = len(arr)//2
     left = merge_sort(arr[:mid])
     right = merge_sort(arr[mid:])
-
     return merge(left, right)
 
 def merge(left, right):
     result = []
     i = j = 0
-
     while i < len(left) and j < len(right):
         if left[i] < right[j]:
             result.append(left[i])
@@ -19,10 +16,8 @@ def merge(left, right):
         else:
             result.append(right[j])
             j = j+1
-            
     result.extend(left[i:])
     result.extend(right[j:])
-
     return result
 
 print(merge_sort([2, 5, 4, 6, 99, 0, 7]))
