4a1e421f281b0713c1deffcb361261062fc6693a
Varun Athithiya <varunathithiya.shenbagaraj@octavebio.com>
2025-08-07 13:16:29 +0530

    clean up

--- python/dsa/sorting_algo/bubble_sort_2.py
+++
@@ -1,8 +0,0 @@
-def bubbleSort(arr):
-    for i in range(len(arr)):
-        for j in range(len(arr)-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-    return arr
-
-print(bubbleSort([2, 4, 5, 6, 99, -10]))
